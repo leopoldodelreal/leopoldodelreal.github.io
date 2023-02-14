c447feb3ea8dba0534ec4fab8e60ce9131f9c116
leopoldodelreal <122302675+leopoldodelreal@users.noreply.github.com>
2023-02-14 13:35:06 -0300

    Add files via upload
    Version Inicial

---
+++ js/codigo.js
@@ -0,0 +1,5 @@
+document.getElementById('boton').onclick = function(){
+    console.log('El nombre fue: '+ document.getElementById('nombre').value);
+    console.log('El email fue: '+ document.getElementById('email').value);
+ 
+}
